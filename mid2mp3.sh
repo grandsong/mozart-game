54443fba171c64c8583dce92f134536f478491dc
Martin Vician <sansom@sansom>
2014-10-05 12:53:37 +0200

    without uncut

--- mid2mp3.sh
+++ mid2mp3.sh
@@ -5,14 +5,16 @@
 mid_dir="./mid"
 mid_ext="MID"
 # Non-cutted mp3
-mp3l_dir="./mp3l"
-mp3l_ext="mp3l"
-# mp3 results
-mp3l_dir="./mp3"
+mp3l_dir="./mp3_uncut"
 mp3l_ext="mp3"
+# mp3 results
+mp3_dir="./mp3"
+mp3_ext="mp3"
 # Tim for cutting
 start="00.07"
 duration="01.83" # in seconds
+# Packages required
+packages=( "timidity" "lame" "ffmpeg" )
 
 ## Functions
 function mid2mp3 {
@@ -20,41 +22,53 @@ function mid2mp3 {
 		echo "ERROR: midi2mp3 function without file!"
 		exit 1;
 	fi
-	mid=$1
-	mp3l="${mid%.*}.mp3l"
-	mp3="${mid%.*}.mp3"
-	echo "- $mid -> $mp3l"
+	filename=$1
+	mid="$mid_dir/$filename.$mid_ext"
+	mp3l="$mp3l_dir/$filename.$mp3l_ext"
+	echo "-- mid2mp3 $mid -> $mp3l"
 	timidity --output-24bit $mid -Ow -o - | lame - -b 64 $mp3l
-	ffmpeg -ss $start -t $duration -i $mp3l $mp3
-	#
 }
 
 function mp3cut {
 	if [ $# -ne 1 ]; then
-		echo "ERROR: midi2mp3 function without file!"
+		echo "ERROR: mp3cut function without file!"
 		exit 1;
 	fi
-	mp3l=$1
-	mp3l="${mid%.*}.mp3l"
-	mp3="${mid%.*}.mp3"
-	echo "- $mid -> $mp3l"
-	timidity --output-24bit $mid -Ow -o - | lame - -b 64 $mp3l
-	ffmpeg -ss $start -t $duration -i $mp3l $mp3
-	#
+	filename=$1
+	mp3l="$mp3l_dir/$filename.$mp3l_ext"
+	mp3="$mp3_dir/$filename.$mp3_ext"
+	echo "-- mp3cut $mp3l -> $mp3"
+	#ffmpeg -ss $start -t $duration -i $mp3l $mp3
+	cp $mp3l $mp3
 }
 
 function all_mid {
-	files=( `ls $dir/*.$ext` )
+	files=( `ls $mid_dir/*.$mid_ext` )
 	for file in ${files[@]}; do
-		#echo "- converting $file"
-		mid2mp3 $file
+		echo "- $file"
+		filename="${file%.*}"
+		filename="${filename##*/}"
+		mid2mp3 $filename
+		mp3cut $filename
 	done
 }
 
 ## Main
 
-rm -rf $dir/*.mp3l
-rm -rf $dir/*.mp3
+missing=0
+for package in ${packages[@]}; do
+	if [ -z "`which $package`" ]; then
+		missing=1
+		echo "ERROR: Missing program $package, please install it first!"
+	fi
+done
+if [ $missing -eq 1 ]; then
+	exit 1
+fi
+
+rm -irf $mp3l_dir
+mkdir $mp3l_dir
+rm -irf $mp3_dir
+mkdir $mp3_dir
 
 all_mid
-#midi2mp3 M1.MID
