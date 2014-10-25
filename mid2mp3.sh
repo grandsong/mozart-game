9b7046bdc9e016f9450952006672d88318e91da4
Martin Vician <martin@vician.cz>
2014-10-25 11:46:58 +0200

    without uncut

--- mid2mp3.sh
+++ mid2mp3.sh
@@ -4,19 +4,11 @@
 # Source mid
 mid_dir="./mid"
 mid_ext="MID"
-# Non-cutted mp3
-mp3l_dir="./mp3_uncut"
-mp3l_ext="mp3"
 # mp3 results
 mp3_dir="./mp3"
 mp3_ext="mp3"
-# Tim for cutting
-start="00.07"
-duration="01.83" # in seconds
-from="00:00:00.10"
-to="00:00:01.60"
 # Packages required
-packages=( "timidity" "lame" "ffmpeg" )
+packages=( "timidity" "lame" )
 
 ## Functions
 function mid2mp3 {
@@ -26,23 +18,9 @@ function mid2mp3 {
 	fi
 	filename=$1
 	mid="$mid_dir/$filename.$mid_ext"
-	mp3l="$mp3l_dir/$filename.$mp3l_ext"
-	echo "-- mid2mp3 $mid -> $mp3l"
-	timidity --output-24bit $mid -Ow -o - | lame - -b 64 $mp3l
-}
-
-function mp3cut {
-	if [ $# -ne 1 ]; then
-		echo "ERROR: mp3cut function without file!"
-		exit 1;
-	fi
-	filename=$1
-	mp3l="$mp3l_dir/$filename.$mp3l_ext"
 	mp3="$mp3_dir/$filename.$mp3_ext"
-	echo "-- mp3cut $mp3l -> $mp3"
-	#ffmpeg -ss $start -t $duration -i $mp3l -c:v copy -c:a copy $mp3
-	mpgsplit $mp3l [$from-$to] -o $mp3
-	#cp $mp3l $mp3
+	echo "-- mid2mp3 $mid -> $mp3"
+	timidity --output-24bit $mid -Ow -o - | lame - -b 64 $mp3
 }
 
 function all_mid {
@@ -52,7 +30,6 @@ function all_mid {
 		filename="${file%.*}"
 		filename="${filename##*/}"
 		mid2mp3 $filename
-		mp3cut $filename
 	done
 }
 
